804d41c5dbd4a388489492d8eeb56dea9310ae17
T-Rit <sor.thyrith@gmail.com>
2026-02-09 17:18:15 +0700

    Adjust text width based on locale Khmer for date scroll view

--- lib/src/widgets/date_scroll_view.dart
+++ lib/src/widgets/date_scroll_view.dart
@@ -52,6 +52,8 @@ class DateScrollView extends StatelessWidget {
 
     if (locale.languageCode == ar) {
       return textWidth + 40;
+    } else if (locale.languageCode == km) {
+      return textWidth + 12;
     }
 
     return textWidth + 8;
